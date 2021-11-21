38db6f5a5b2d15691e7c02f0dddf93c65d2ea087
Andres2020Leonardo <andruuescobar@gmail.com>
2021-11-21 15:26:33 -0500

    version 2.0

--- src/components/FormularioLogin.jsx
+++ src/components/FormularioLogin.jsx
@@ -17,19 +17,19 @@ export default function FormularioLogin() {
           
         };
    return (
-    <div className="container col-3  position-absolute top-50 start-50 translate-middle shadow-lg p-3 mb-5 bg-body rounded"  >
+    <div className="container col-sm-3 m-x-auto  position-absolute top-50 start-50 translate-middle shadow-lg p-3 mb-5 bg-body rounded"  >
             <form onSubmit={loginform}>
                 <div className="mb-3">
-                    <label className="form-label"><h4>Documento</h4></label>
-                    <input type="number" className="form-control" id="LoginDocumento" required onChange={e=>setDocumento(e.target.value)}/>
+                    <label className="form-label col-sm"><h4 className="col-sm">Documento</h4></label>
+                    <input type="number" className="form-control col-sm" id="LoginDocumento" required onChange={e=>setDocumento(e.target.value)}/>
                    
                 </div>
                 <div className="mb-3">
-                    <label  className="form-label"><h4>Password</h4></label>
+                    <label  className="form-label col-sm"><h4 className="col-sm">Password</h4></label>
                     <input type="password" className="form-control" id="LoginPassword" required onChange={e=>setPassword(e.target.value)}/>
                 </div>
                 
-                <button type="submit" className="btn  mt-3 w-50" style={styleBoton}><h5>Login</h5></button>
+                <button type="submit" className="btn  mt-3 w-50 w-sm-75" style={styleBoton}><h5 className="col-sm">Login</h5></button>
             </form>
        
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,23 +3,23 @@ import '../navbar.css';
 export default function Navbar() {
     return (
 
-        <nav className="navbar navbar-expand-lg navbar-light shadow p-3 mb-5 rounded" id="nav">
-            <div className="col-11">
-                <img src="icono.png" alt="icon " className="" id="icoNavbar" />
-                <h3 className="text-success aling-left">Inventario Panelera J</h3>
-            </div>
-
-
-            <div className="collapse navbar-collapse col" id="" >
-            <button type="button" className="btn btn-outline-success" id="contentNavbarUser">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-person-square col" viewBox="0 0 16 16">
+        <nav className="navbar navbar-expand-lg navbar-light shadow p-3  rounded" id="nav">
+            <div className="col-sm-11 " id="container-navbar">
+                <div className="" id="divImg"><img src="icono.png" alt="icon " className="" id="icoNavbar" /></div>
+                <div className="h3 text-success"  id="divNombre">Inventario Panelera J</div>
+                <div className="" id="divBoton"><button type="button" className="btn btn-success" id="botonNavbar">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-person-square " viewBox="0 0 16 16">
                         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                         <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12z" />
                     </svg>
-                </button>
-
-
+                </button></div>
+                
+                
+                
             </div>
+
+
+          
         </nav>
 
     )
